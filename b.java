09b6411d43f2dafd717ad4b30236addcf435cc4a
hessedihyun <88070549+hessedihyun@users.noreply.github.com>
2023-11-09 09:21:18 +0900

    updated java

--- b.java
+++ b.java
@@ -1,8 +1,10 @@
-'use strict'
+public class a{
+    public static void main(String[] args){
 
-for ( int i=1 ; 0<i<9 ; i++) {
-    for ( int k=1 ; 0<k<10 ; k++) {
+    for ( int i=1 ; 0<i<9 ; i++) {
+        for ( int k=1 ; 0<k<10 ; k++) {
         System.out.println((i+1) + " x " + k " = " k(i+1));
+            }
+        }
     }
 }
-
--- index.js
+++ index.js
@@ -1,22 +1,7 @@
-'use strict';
+'use strict'
 
-let nn, score1, score2, score3, avgscore, grade;
-
-nn = prompt('이름:');
-score1 = prompt('국어 점수 :');
-score2 = prompt('영어 점수 :');
-score3 = prompt('수학 점수 :');
-avgscore = (score1 + score2 + score3)/3
-
-if (avgscore >= 90) {
-    grade = "A";
-} else if (avgscore >= 80) {
-    grade = "B";
-} else if (avgscore >= 70) {
-    grade = "C";
-} else {
-    grade = "D";
+for ( int i=1 ; 0<i<9 ; i++) {
+    for ( int k=1 ; 0<k<10 ; k++) {
+    System.out.println((i+1) + " x " + k " = " + k(i+1));
+    }
 }
-
-alert(`<성적>
-학생명 : ${nn}, 학점: ${grade}, 평균: ${avgscore}`);
